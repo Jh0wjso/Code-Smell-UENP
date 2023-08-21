e76dd30ab27487fd79864af950e9a2ce02bcccc4
Geovane Roberto Rigonato <geovane.rigonato.gil@gmail.com>
2023-08-20 21:53:15 -0300

    feat CodeSmell Main

--- Locadora/src/br/es/Main.java
+++ Locadora/src/br/es/Main.java
@@ -1,10 +1,7 @@
 package br.es;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        SistemaLocadora sistema = new SistemaLocadora();
-        sistema.iniciarSistema();
+        SistemaLocadora.iniciarSistema();
     }
 }
